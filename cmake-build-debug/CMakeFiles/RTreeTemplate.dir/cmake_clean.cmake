9aa8eff6d617b96bffe1d9614d2a80741e43e4d8
wy0705 <8617935+wy0705@user.noreply.gitee.com>
2021-10-18 17:51:41 +0800

    ‘111’

---
+++ cmake-build-debug/CMakeFiles/RTreeTemplate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTreeTemplate.dir/MemoryTest.cpp.o"
+  "RTreeTemplate"
+  "RTreeTemplate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTreeTemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
